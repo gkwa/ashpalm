ab651cca5e4843f54d356ccd08f1ecfbfaf5963a
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-15 10:27:32 -0700

    Protect againsta missing workding directory

--- ashpalm.go
+++ ashpalm.go
@@ -4,12 +4,20 @@ import (
 	"bytes"
 	"log/slog"
 	"os/exec"
+
+	mymazda "github.com/taylormonacelli/forestfish/mymazda"
 )
 
-func RunCmd(cmd *exec.Cmd) (int, string, string) {
+func RunCmd(cmd *exec.Cmd, cwd string) (int, string, string) {
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
+	cmd.Dir = cwd
+
+	if !mymazda.DirExists(cwd) {
+		slog.Error("can't change to directory", "directory", cwd)
+		return 1, "", ""
+	}
 
 	err := cmd.Run()
 
