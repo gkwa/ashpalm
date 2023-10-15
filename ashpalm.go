34ed093ac95cee62c89f1cf1b9b467b743da8d62
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-15 10:05:00 -0700

    Allow caller to inspect stdout, err

--- ashpalm.go
+++ ashpalm.go
@@ -6,7 +6,7 @@ import (
 	"os/exec"
 )
 
-func RunCmd(cmd *exec.Cmd) int {
+func RunCmd(cmd *exec.Cmd) (int, string, string) {
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
@@ -15,19 +15,16 @@ func RunCmd(cmd *exec.Cmd) int {
 
 	if err == nil {
 		slog.Debug("command executed successfully", "cmd", cmd.String())
-		return 0
+		return 0, stdout.String(), stderr.String()
 	}
-
-	slog.Debug("stdout", "cmd", cmd.String(), "str", stdout.String())
-	slog.Debug("stderr", "cmd", cmd.String(), "str", stderr.String())
 
 	exitErr, ok := err.(*exec.ExitError)
 	if ok {
 		slog.Error("command had error", "cmd", cmd.String(), "code", exitErr.ExitCode())
-		return exitErr.ExitCode()
+		return exitErr.ExitCode(), stdout.String(), stderr.String()
 	}
 
 	slog.Error("not ok", "error", err.Error())
 
-	return 1
+	return 1, stdout.String(), stderr.String()
 }
