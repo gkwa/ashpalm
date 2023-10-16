317dfaa624cf90c3d2de200c0888232492338cdd
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-16 06:42:03 -0700

    Don't log message twice; once in module and another in clint

--- ashpalm.go
+++ ashpalm.go
@@ -22,17 +22,13 @@ func RunCmd(cmd *exec.Cmd, cwd string) (int, string, string) {
 	err := cmd.Run()
 
 	if err == nil {
-		slog.Debug("command executed successfully", "cmd", cmd.String())
 		return 0, stdout.String(), stderr.String()
 	}
 
 	exitErr, ok := err.(*exec.ExitError)
 	if ok {
-		slog.Error("command had error", "cmd", cmd.String(), "code", exitErr.ExitCode())
 		return exitErr.ExitCode(), stdout.String(), stderr.String()
 	}
-
-	slog.Error("not ok", "error", err.Error())
 
 	return 1, stdout.String(), stderr.String()
 }
